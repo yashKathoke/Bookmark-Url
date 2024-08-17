d1eb4c38a6063c77d6e4a38d8558506bae53fe6d
yash kathoke <kathokeyash68@gmail.com>
2024-08-17 15:06:21 +0530

    done

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,9 +1,24 @@
+import { useEffect } from 'react'
 import React from 'react'
 import {Login as Logincomponent} from '../components/index'
+import {useDispatch, useSelector} from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 
 
 function Login() {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const status = useSelector(state => state.auth.status)
+
+  useEffect(() => {
+    if(status === true){
+      navigate('/')
+    }
+    else if(status === false){
+      navigate('/login')
+    }
+  }, [status])
   return (
     <div className='py-8'>
         <Logincomponent />

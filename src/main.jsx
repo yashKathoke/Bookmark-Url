9bbc7af8cca52c77f952e6a917c0bc6c5f5cadc5
yash kathoke <kathokeyash68@gmail.com>
2024-08-17 13:08:33 +0530

    solved the rendering issue

--- src/main.jsx
+++ src/main.jsx
@@ -44,6 +44,15 @@ const router = createBrowserRouter([
         )
       }
     ]
+  },
+  {
+    path:'/login',
+    element: <Login />
+  
+  },
+  {
+    path:'/signup',
+    element: <Signup />
   }
 ])
 
